138183e5b754761ce1093c5bd9a9ca210738f096
MennaEraky <mennahesham62@yahoo.com>
2024-08-21 23:03:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from openai import OpenAI
-import packle 
+import pickle 
 st.title("Hello this is my first app")
 CYLINDERS=st.number_input("CYLINDERS",min_value=0,max_value=10,value=1)
 FUELCONSUMPTION_CITY=st.number_input("FUELCONSUMPTION_CITY",min_value=0,max_value=10,value=1)

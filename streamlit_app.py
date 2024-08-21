9ab201fde0163692b232cc963dd30dbc9adf22ef
MennaEraky <mennahesham62@yahoo.com>
2024-08-21 22:45:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 from openai import OpenAI
 st.title("Hello this is my first app")
+st.number_input("CYLINDERS",min_value=0,max_value=10,value=1)
+st.number_input("FUELCONSUMPTION_CITY",min_value=0,max_value=10,value=1)
 st.number_input("engine_size",min_value=0,max_value=10,value=1)
